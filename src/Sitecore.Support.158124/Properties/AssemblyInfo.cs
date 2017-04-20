90e99f0d22c49be94bc45ba7210f48e462492ef5
Alexey Lunev <alu@sitecore.net>
2017-04-20 17:23:55 +0300

    Initial commit

---
+++ src/Sitecore.Support.158124/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.158124")]
+[assembly: AssemblyProduct("Sitecore.Support.158124")]
+[assembly: ComVisible(false)]
